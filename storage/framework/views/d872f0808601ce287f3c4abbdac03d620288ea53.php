6ba4ab77ce3c3275e27d74180b4e1afebe7d9761
Aldy Tamara <aldytamara@gmail.com>
2019-02-14 21:37:31 +0700

    storage and resources
    storag, resources, and routes

---
+++ storage/framework/views/d872f0808601ce287f3c4abbdac03d620288ea53.php
@@ -0,0 +1,12 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="d-flex flex-row justify-content-center">
+ <div class="col-md-6 text-center">
+ <div class="alert alert-danger">
+ <h1>404</h1>
+ <h4><?php echo e($exception->getMessage()); ?></h4>
+ </div>
+ </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
